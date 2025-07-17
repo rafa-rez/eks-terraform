cd22959fd32723dbf8e6fa790672af5647ef105d
rafa-rez <rafaelrez2004@gmail.com>
2025-07-17 16:20:23 -0300

    :sparkles: feat: config do pre-commit

--- provider.tf
+++ provider.tf
@@ -1,9 +1,14 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "6.3.0"
     }
+  }
+  backend "s3" {
+    bucket = "rez-rafa-eks-s3"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
   }
 }
 
