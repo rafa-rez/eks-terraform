3cedbada571d072b4f5343e22fbc29aa4966e5c1
rafa-rez <rafaelrez2004@gmail.com>
2025-07-21 16:57:00 -0300

    :sparkles: feat: rtb pública - igw + rtb_assoc

---
+++ igw.tf
@@ -0,0 +1,28 @@
+resource "aws_internet_gateway" "eks-igw" {
+  vpc_id = aws_vpc.eks_vpc.id
+
+  tags = merge(
+    local.tags,
+    {
+      Name = "EKS-IGW"
+
+    }
+  )
+}
+
+resource "aws_route_table" "eks-public-route_table" {
+  vpc_id = aws_vpc.eks_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.eks-igw.id
+  }
+
+  tags = merge(
+    local.tags,
+    {
+      Name = "public-route_table"
+
+    }
+  )
+}
--- public.tf
+++ public.tf
@@ -28,3 +28,13 @@ resource "aws_subnet" "eks_subnet_pub_1b" {
     }
   )
 }
+
+resource "aws_route_table_association" "eks_rtb_assoc_1a" {
+  subnet_id      = aws_subnet.eks_subnet_pub_1a.id
+  route_table_id = aws_route_table.eks-public-route_table.id
+}
+
+resource "aws_route_table_association" "eks_rtb_assoc_1b" {
+  subnet_id      = aws_subnet.eks_subnet_pub_1b.id
+  route_table_id = aws_route_table.eks-public-route_table.id
+}
