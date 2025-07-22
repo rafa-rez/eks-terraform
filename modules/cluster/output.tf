0d5108daaef669633cc6e5ee32c8303b8b837c9e
rafa-rez <rafaelrez2004@gmail.com>
2025-07-22 16:01:59 -0300

    :sparkles: feat: add do module 'managed-node-group'

--- modules/cluster/output.tf
+++ modules/cluster/output.tf
@@ -1,3 +1,7 @@
 output "eks_vpc_config" {
   value = aws_eks_cluster.eks_cluster.vpc_config
 }
+
+output "cluster_name" {
+  value = aws_eks_cluster.eks_cluster.id
+}
---
+++ modules/managed-node-group/variables.tf
@@ -0,0 +1,19 @@
+variable "tags" {
+  type        = map(any)
+  description = "Tags to be added to AWS resources"
+}
+
+variable "cluster_name" {
+  type        = string
+  description = "EKS cluster name to create MNG"
+}
+
+variable "subnet_private_1a" {
+  type        = string
+  description = "Subnet ID from AZ 1a"
+}
+
+variable "subnet_private_1b" {
+  type        = string
+  description = "Subnet ID from AZ 1b"
+}
